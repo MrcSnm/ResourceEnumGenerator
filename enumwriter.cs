725fc67286b0b3dcf54715e2cca0b02f6b4b2c60
MrcSnm <msnmancini@hotmail.com>
2019-10-07 23:45:13 -0300

    Added options for formatting as a JSON

---
+++ enumwriter.cs
@@ -0,0 +1,27 @@
+{
+	bin :
+	{
+		"ENUMWRITER.CLASS" : EnumWriter.class,
+		"ENUMWRITER$1.CLASS" : EnumWriter$1.class,
+		"RESOURCEENUMGENERATOR.CLASS" : ResourceEnumGenerator.class,
+		"RESOURCEENUMGENERATOR$2.CLASS" : ResourceEnumGenerator$2.class,
+		"RESOURCEENUMGENERATOR$1.CLASS" : ResourceEnumGenerator$1.class
+	},
+	. :
+	{
+		"ENUMWRITER.CS" : enumwriter.cs,
+		"ENUMWRITER.C" : enumwriter.c,
+	},
+	src :
+	{
+	},
+	test :
+	{
+		"GARBAGE.TXT" : garbage.txt,
+		"GARBAGE___COPIA.TXT" : garbage___Copia.txt,
+		"GARBAGE___COPIA___COPIA.TXT" : garbage___Copia___Copia.txt
+	},
+	ou :
+	{
+	},
+}
--- src/EnumWriter.java
+++ src/EnumWriter.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -14,16 +11,26 @@ public class EnumWriter
 {
 	public String path = "./enumwriter.cs";
 	
-	private String classDeclarator = "public class";
+	private String classDeclarator = "public class ";
 	private String className = "class1";
 	public String pathToWatch = "./";
+
 	private String surroundEnumConstsWith = "";
-	private String enumDeclarator = "public enum";
+	private String enumDeclarator = "public enum ";
+	private String postEnumDeclaration = "";
+	private String afterEnumLastBracket = "";
+	private boolean enumConstToUppercase = false;
+	private boolean enumToUppercase = false;
+
 	private String assignSymbol = "=";
+
 	private ArrayList<String> toIgnore = new ArrayList<String>();
-	private String ignoredExtensions = ".config, .java";
+	private ArrayList<String> pathsToIgnore = new ArrayList<String>();
+	private String ignoredExtensions = ".config, .java, .git, .classpath, .project";
+	private String ignoredPaths = ".git, .vscode";
+
 	private Thread t;
-	private boolean willJumpLine = true;
+	private boolean willUseAssign = false;
 	public boolean isWriting = false;
 	
 	public boolean isRunning = true;
@@ -72,10 +79,15 @@ public class EnumWriter
 		config+= "CLASS_NAME= " + className+"\n";
 		config+= "CLASS_DECLARATOR= " + classDeclarator+"\n";
 		config+= "ENUM_DECLARATOR= " + enumDeclarator+"\n";
+		config+= "POST_ENUM_DECLARATION= " + postEnumDeclaration+"\n";
+		config+= "AFTER_ENUM_LAST_BRACKET= " + afterEnumLastBracket+"\n";
 		config+= "ENUM_CONST_SURROUND_WITH= " + surroundEnumConstsWith+"\n";
+		config+= "ENUM_TO_UPPERCASE= " + enumToUppercase+"\n";
+		config+= "ENUM_CONST_TO_UPPERCASE= " + enumConstToUppercase+"\n";
 		config+= "ASSIGN_SYMBOL= " + assignSymbol+"\n";
-		config+= "WILL_JUMP_LINE= " + willJumpLine+"\n";
+		config+= "WILL_USE_ASSIGN= " + willUseAssign+"\n";
 		config+= "IGNORE_EXTENSIONS= " + ignoredExtensions+"\n";
+		config+= "IGNORE_PATHS= " + ignoredPaths+"\n";
 		
 		
 		pw.write(config);
@@ -93,12 +105,17 @@ public class EnumWriter
 			className = getContent(list, "CLASS_NAME= ");
 			classDeclarator = getContent(list, "CLASS_DECLARATOR= ");
 			enumDeclarator = getContent(list, "ENUM_DECLARATOR= ");
+			postEnumDeclaration = getContent(list, "POST_ENUM_DECLARATION= ");
+			afterEnumLastBracket = getContent(list, "AFTER_ENUM_LAST_BRACKET= ");
 			surroundEnumConstsWith = getContent(list, "ENUM_CONST_SURROUND_WITH= ");
+			enumToUppercase = getBool(list, "ENUM_TO_UPPERCASE= ");
+			enumConstToUppercase = getBool(list, "ENUM_CONST_TO_UPPERCASE= ");
 			assignSymbol = getContent(list, "ASSIGN_SYMBOL= ");
-			willJumpLine = getBool(list, "WILL_JUMP_LINE= ");
+			willUseAssign = getBool(list, "WILL_USE_ASSIGN= ");
 			toIgnore = getStrings(list, "IGNORE_EXTENSIONS= ");
 			ignoredExtensions = getContent(list, "IGNORE_EXTENSIONS= ");
-			
+			pathsToIgnore = getStrings(list, "IGNORE_PATHS= ");
+			ignoredPaths = getContent(list, "IGNORE_PATHS= ");
 		}
 		else
 			generateDefaultFormatFile();
@@ -110,12 +127,18 @@ public class EnumWriter
 		isUpdateScheduled = true;
 	}
 
-	private boolean checkIgnored(String str)
+	private boolean checkIgnored(String str, ArrayList<String> listToIgnore)
 	{
-		for(int i = 0, len = toIgnore.size(); i < len; i++)
-			if(str.contains(toIgnore.get(i)))
+		for(int i = 0, len = listToIgnore.size(); i < len; i++)
+		{
+			if(str.contains(listToIgnore.get(i)))
+			{
+				System.out.println(str + " was ignored because of the " + listToIgnore.get(i) + " ignore");
 				return true;
+			}
+		}
 		return false;
+
 	}
 	
 	
@@ -128,23 +151,48 @@ public class EnumWriter
 			System.out.println("Starting to write file");
 			String code = "";
 			PrintWriter pw = new PrintWriter(path);
-			code+= classDeclarator + " " + className + "\n{\n";
+			if(!classDeclarator.equals("") && !className.equals(""))
+				code+= classDeclarator + className + "\n{\n";
+			else
+				code+="{\n";
+
+			for(int i = 0, len = paths.size(); i < len; i++)
+			{
+				if(checkIgnored(paths.get(i).toString(), pathsToIgnore))
+				{
+					paths.remove(i);
+					i--;
+					len--;
+				}
+			}
+
 			for(Path path : paths)
 			{
 				File[] files = path.toFile().listFiles(File::isFile);
-				code+= "\t" + enumDeclarator + " " + path.toFile().getName() + "\n\t{\n";
-				for(int i = 0, len = files.length; i < len; i++)
+				if(enumToUppercase)
+					code+= "\t" + enumDeclarator + path.toFile().getName().toUpperCase() + postEnumDeclaration + "\n\t{\n";
+				else
+					code+= "\t" + enumDeclarator + path.toFile().getName() + postEnumDeclaration + "\n\t{\n";
+				if(files != null)
 				{
-					String enumConstant = files[i].getName();
-					if(checkIgnored(enumConstant))
-						continue;
-					enumConstant = enumConstant.replaceAll("[\\s|\\-]", "_");
-					code+= "\t\t" + surroundEnumConstsWith + enumConstant + surroundEnumConstsWith;
-					if(i != len -1)
-						code+= ",";
-					code+= "\n";
+					for(int i = 0, len = files.length; i < len; i++)
+					{
+						String enumConstant = files[i].getName();
+						if(checkIgnored(enumConstant, toIgnore))
+							continue;
+						enumConstant = enumConstant.replaceAll("[\\s|\\-]", "_");
+						if(enumConstToUppercase)
+							code+= "\t\t" + surroundEnumConstsWith + enumConstant.toUpperCase() + surroundEnumConstsWith;
+						else
+							code+= "\t\t" + surroundEnumConstsWith + enumConstant + surroundEnumConstsWith;
+						if(willUseAssign)
+							code+= " " + assignSymbol + " " + enumConstant;
+						if(i != len -1)
+							code+= ",";
+						code+= "\n";
+					}
 				}
-				code+= "\t}\n";
+				code+= "\t}" + afterEnumLastBracket + "\n";
 			}
 			code+= "}";
 			pw.write(code);
@@ -170,17 +218,23 @@ public class EnumWriter
 		if(str == null)
 			return null;
 		String strBuffer = "";
-		ArrayList<String> res = new ArrayList<String >();
+
+		System.out.println(str);
+		ArrayList<String> res = new ArrayList<String>();
 		for(int i = 0, len = str.length(); i < len; i++)
 		{
-			if(str.charAt(i) != ',')
+			if(str.charAt(i) == ' ')
+				continue;
+			else if(str.charAt(i) != ',')
 				strBuffer+= str.charAt(i);
-			else
+			else 
 			{
 				res.add(strBuffer);
 				strBuffer = "";
 			}
 		}
+		if(!strBuffer.equals(""))
+			res.add(strBuffer);
 		return res;
 
 	}
@@ -200,7 +254,4 @@ public class EnumWriter
 			return Integer.parseInt(str);
 		return 0;
 	}
-	
-	
-
 }
